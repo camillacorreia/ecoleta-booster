15cadbf5ac03dc3af665abad68083b7606384aca
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-22 22:28:13 -0300

    Listagem dos itens
    --transpileOnly --ignore-watch node_modules  RECARREGAR MAIS RÁPIDO

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
