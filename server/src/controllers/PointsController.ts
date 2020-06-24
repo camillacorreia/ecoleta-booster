380706f336b51066119e8717199694c87c28194c
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-24 17:41:28 -0300

    Listar pontos (filtro por estado/cidade/items)
    O cadastro dos pontos de coleta foi realmente consertado adicionando o "trx.commit"

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express'
 import knex from '../database/connection'
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct() // para retonar pontos diferentes
+      .select('points.*');
+
+    return res.json(points);
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params
 
@@ -55,6 +73,8 @@ class PointsController {
     })
 
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
 
     return res.json({
       id: point_id,
