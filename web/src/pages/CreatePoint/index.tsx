754c23683cefdde2c06549792e94d0165ad97d1e
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-25 17:59:02 -0300

    Buscando itens de coleta da API
    npm install axios
    AXIOS: Permite fazer requisições para o backend NodeJS

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
+
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
 
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
+
 const CreatePoint = () => {
+
+    const [items, setItems] = useState<Item[]>([])
+
+    useEffect(() => {
+        api.get("items").then(res => {
+            setItems(res.data)
+        });
+      }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -96,20 +113,16 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/baterias.svg" alt="Teste"/>
-                            <span>Pilhas e Baterias</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        {items.map(item => {
+                            return (
+                                <li
+                                key={item.id}
+                                >
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </fieldset>
 
