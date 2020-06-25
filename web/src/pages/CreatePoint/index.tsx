d2a16f6dbb0ea18ff19d63f8dc3ae638257052e8
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-25 17:37:32 -0300

    Integrando mapa no formulário
    npm install leaflet react-leaflet
    npm install @types/react-leaflet -D

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
-
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
@@ -61,6 +61,16 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-12.9638663, -38.4994626]} zoom={15} onClick>
+
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-12.9638663, -38.4994626]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
