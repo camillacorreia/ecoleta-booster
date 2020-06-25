14adbc55a5d757f475518d5d3fb5c1d6fc519b9c
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-25 19:24:17 -0300

    Cadastro de ponto de coleta na API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -25,6 +25,7 @@ interface IBGECityResponse {
 }
 
 const CreatePoint = () => {
+    const history = useHistory();
 
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUfs] = useState<string[]>([]);
@@ -110,6 +111,33 @@ const CreatePoint = () => {
         else {
           setSelectedItems([...selectedItems, id])
         }
+    }
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+    
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+    
+        const data = new FormData()
+    
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+    
+        await api.post('points', data);
+    
+        alert('Ponto de coleta criado');
+    
+        history.push('/');
     }
 
     return (
